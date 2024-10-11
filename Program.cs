31849491f95374f24ab5d95f3e0a14402169e475
andre.hilario <andre.hcosta@hotmail.com>
2024-10-10 21:41:05 -0300

    usuario define algums atributos pelo console.

--- Program.cs
+++ Program.cs
@@ -6,18 +6,47 @@ Console.OutputEncoding = Encoding.UTF8;
 // Cria os modelos de hóspedes e cadastra na lista de hóspedes
 List<Pessoa> hospedes = new List<Pessoa>();
 
-Pessoa p1 = new Pessoa(nome: "Hóspede 1");
-Pessoa p2 = new Pessoa(nome: "Hóspede 2");
-
-hospedes.Add(p1);
-hospedes.Add(p2);
-
 // Cria a suíte
-Suite suite = new Suite(tipoSuite: "Premium", capacidade: 2, valorDiaria: 30);
+Suite suite = new Suite();
+Console.WriteLine("Informe a Suite desejada.\n*1 - Master.\n*2 - Premium.\n*3 - Standart.");
+int seletorSuite = int.Parse(Console.ReadLine());
+
+switch (seletorSuite)
+{
+    case 1:
+        suite.TipoSuite = "Master";
+        suite.ValorDiaria = 360;
+        suite.Capacidade = 6;
+        break;
+    case 2:
+        suite.TipoSuite = "Premium";
+        suite.ValorDiaria = 320;
+        suite.Capacidade = 4;
+        break;
+    default:
+        suite.TipoSuite = "Standart";
+        suite.ValorDiaria = 260;
+        suite.Capacidade = 2;
+        break;
+}
 
 // Cria uma nova reserva, passando a suíte e os hóspedes
-Reserva reserva = new Reserva(diasReservados: 5);
+Reserva reserva = new Reserva();
+Console.WriteLine("Informe a quantidade de dias a serem reservados.");
+reserva.DiasReservados = int.Parse(Console.ReadLine());
 reserva.CadastrarSuite(suite);
+
+Console.WriteLine("Informe a quantidade de hóspedes");
+int entradaHospedes = int.Parse((Console.ReadLine()));
+
+for (int i = 0; i < entradaHospedes; i++)
+{
+    Console.WriteLine("Informe o nome do hospede.");
+    Pessoa pessoa = new Pessoa();
+    pessoa.Nome = Console.ReadLine();
+    hospedes.Add(pessoa);
+}
+
 reserva.CadastrarHospedes(hospedes);
 
 // Exibe a quantidade de hóspedes e o valor da diária
